093dd20260a5291b3bb2dcb2a0f7d0b05fd054e0
Ben Krepp <bkrepp@ctps.org>
2022-09-26 13:22:34 -0400

    Fixes from 2nd round of prototyping.

--- npmrds_utilities.py
+++ npmrds_utilities.py
@@ -2,16 +2,16 @@
 #
 import datetime
 
-tstamp = "1/2/2019 10:15"
+tstamp = "2019-01-02 10:15"
 
 # Parse 'date' part of measurement_tstamp field
 tmp = tstamp.split(' ')
 datepart = tmp[0]
 timepart = tmp[1]
-tmp2 = datepart.split('/')
-mo = int(tmp2[0])
-dy = int(tmp2[1])
-yr = int(tmp2[2])
+tmp2 = datepart.split('-')
+mo = int(tmp2[1])
+dy = int(tmp2[2])
+yr = int(tmp2[0])
 # Determine day-of-week
 dtemp = datetime.date(yr, mo, dy)
 # The following call returns 0 - 6, for Mon - Sun
@@ -32,7 +32,7 @@ nhpp_weekend_6_8 =  4
 
 # Determine NHPP_Period: the value returned depends upon day_of_week and hour.
 # Note that 'hour' values are given using the 24-hour (not 12-hour) clock.
-def get_nhpp_period(day_of_week, hour):
+def get_nhpp_period(dow, hour):
 	retval = 0
 	if (dow == 5 or dow == 6):
 		if hour >=6 and hour < 20:
@@ -54,8 +54,8 @@ def get_nhpp_period(day_of_week, hour):
 
 ritis_df['datepart'] = ritis_df.apply(lambda row : row['measurement_tstamp'].split(' ')[0], axis=1)
 ritis_df['timepart'] = ritis_df.apply(lambda row : row['measurement_tstamp'].split(' ')[1], axis=1)
-ritis_df['mo'] = ritis_df.apply(lambda row : int(row['datepart'].split('/')[0]), axis=1)
-ritis_df['dy'] = ritis_df.apply(lambda row : int(row['datepart'].split('/')[1]), axis=1)
+ritis_df['mo'] = ritis_df.apply(lambda row : int(row['datepart'].split('-')[1]), axis=1)
+ritis_df['dy'] = ritis_df.apply(lambda row : int(row['datepart'].split('-')[2]), axis=1)
 ritis_df['dow'] = ritis_df.apply(lambda row : datetime.date(2019, row['mo'], row['dy']).weekday(), axis=1)
-ritis_df['hour'] = ritis_df.apply(lambda : row int(row['timepart'].split(':')[0]), axis=1)
+ritis_df['hour'] = ritis_df.apply(lambda row: int(row['timepart'].split(':')[0]), axis=1)
 ritis_df['nhpp_period'] = ritis_df.apply(lambda row : get_nhpp_period(row['dow'], row['hour']), axis=1)
